55e640a6a8089e4f638ce4b386384d00f014b85d
ahmadxgani <ahmadxgani01@gmail.com>
2023-03-26 21:19:15 +0700

    feat: add migration table

---
+++ internal/db/migration/000001_init_schema.up.sql
@@ -0,0 +1,49 @@
+CREATE TABLE "accounts" (
+  "id" smallint GENERATED ALWAYS AS IDENTITY UNIQUE, 
+  "username" varchar(16) NOT NULL UNIQUE,
+  "password" varchar NOT NULL,
+  "full_name" varchar(24) NOT NULL,
+  "image" varchar,
+  "created_at" timestamptz NOT NULL DEFAULT now(),
+  "updated_at" timestamptz NOT NULL DEFAULT now()
+);
+
+CREATE TABLE "posts" (
+  "id" smallserial PRIMARY KEY,
+  "title" varchar(25) NOT NULL,
+  "content" varchar NOT NULL,
+  "slug" varchar NOT NULL,
+  "draft" boolean NOT NULL DEFAULT FALSE,
+  "likes" smallint NOT NULL DEFAULT 0,
+  "account_id" smallint NOT NULL,
+
+  CONSTRAINT "unique_title_slug" UNIQUE ("title", "slug"),
+  CONSTRAINT "fk_account" FOREIGN KEY ("account_id") REFERENCES "accounts" ("id")
+);
+
+CREATE TABLE "tags" (
+  "id" smallint GENERATED ALWAYS AS IDENTITY UNIQUE, 
+  "name" varchar NOT NULL UNIQUE DEFAULT 'uncategorized' 
+); 
+
+CREATE TABLE "comments" (
+  "id" smallserial PRIMARY KEY,
+  "content" varchar NOT NULL,
+  "username" varchar NOT NULL,
+  "post_id" smallint NOT NULL,
+  "account_id" smallint NOT NULL,
+
+  CONSTRAINT "fk_post" FOREIGN KEY ("post_id") REFERENCES "posts" ("id"),
+  CONSTRAINT "fk_account" FOREIGN KEY ("account_id") REFERENCES "accounts" ("id")
+);
+
+CREATE TABLE "tag_posts" (
+  "id" smallserial PRIMARY KEY,
+  "tag_id" smallint NOT NULL,
+  "post_id" smallserial NOT NULL,
+
+  CONSTRAINT "unique_tag_post" UNIQUE ("tag_id", "post_id"),
+  CONSTRAINT "fk_tag" FOREIGN KEY ("tag_id") REFERENCES "tags" ("id"),
+  CONSTRAINT "fk_post" FOREIGN KEY ("post_id") REFERENCES "posts" ("id")
+);
+
