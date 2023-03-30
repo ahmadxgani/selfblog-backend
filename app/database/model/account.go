043022cf1f36258c7deb1ce7ea46618401196e59
ahmadxgani <ahmadxgani01@gmail.com>
2023-03-30 22:37:59 +0700

    fix: resolver field with workaround

--- app/database/model/account.go
+++ app/database/model/account.go
@@ -1,16 +1,12 @@
 package app
 
-import (
-	"go-blog/app/database"
-
-	"github.com/graph-gophers/graphql-go"
-)
+import "go-blog/app/database"
 
 type Account struct {
 	Username  string
 	Fullname  string
 	Image     *string
-	CreatedAt graphql.Time
+	CreatedAt string
 }
 
 var err error
@@ -18,12 +14,7 @@ var err error
 func ReadAccount(id uint8) (*Account, error) {
 	var acc *Account = new(Account)
 
-	// fmt.Println(app.DB == nil)
-	// err = errors.New("test")
-	// return &Account{}, err
-
-	// err = app.DB.QueryRow("SELECT username, full_name, image, created_at FROM accounts WHERE id=$1", id).Scan(&acc.Username, &acc.Fullname, sql.NullString{String: *&acc.Image, Valid: true}, &acc.CreatedAt)
-	err = app.DB.QueryRow("SELECT username, full_name, image FROM accounts WHERE id=$1", id).Scan(&acc.Username, &acc.Fullname, &acc.Image)
+	err = app.DB.QueryRow("SELECT username, full_name, image, created_at FROM accounts WHERE id=$1", id).Scan(&acc.Username, &acc.Fullname, &acc.Image, &acc.CreatedAt)
 	if err != nil {
 		return &Account{}, err
 	}
