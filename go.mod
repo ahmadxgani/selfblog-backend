233363266040e7ee5db96d46e9f66437beea9c0c
ahmadxgani <ahmadxgani01@gmail.com>
2023-06-11 19:03:49 +0700

    chore: add cors

--- go.mod
+++ go.mod
@@ -7,3 +7,5 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.7
 )
+
+require github.com/rs/cors v1.9.0 // indirect
--- server/main.go
+++ server/main.go
@@ -10,12 +10,15 @@ import (
 	graphql "github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
 	_ "github.com/lib/pq"
+	"github.com/rs/cors"
 )
 
 func main() {
 	opts := []graphql.SchemaOpt{graphql.UseFieldResolvers()}
+	mux := http.NewServeMux()
 	schema := graphql.MustParseSchema(sdl.Schema, &app.Resolver{}, opts...)
-	http.Handle("/query", &relay.Handler{Schema: schema})
-	http.HandleFunc("/graphiql", func(w http.ResponseWriter, r *http.Request) { w.Write(sdl.Page) })
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	mux.Handle("/query", &relay.Handler{Schema: schema})
+	mux.HandleFunc("/graphiql", func(w http.ResponseWriter, r *http.Request) { w.Write(sdl.Page) })
+	handler := cors.New(cors.Options{AllowedOrigins: []string{"https://studio.apollographql.com"}}).Handler(mux)
+	log.Fatal(http.ListenAndServe(":8080", handler))
 }
