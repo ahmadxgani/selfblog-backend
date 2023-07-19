f8bffb77cfe67be4c60bcacb7ea54ca73bfe27a0
ahmadxgani <ahmadxgani01@gmail.com>
2023-07-19 14:42:26 +0700

    chore: refactor structure

--- server/main.go
+++ server/main.go
@@ -1,17 +1,41 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	app "go-blog/app/controller"
 	"go-blog/pkg/sdl"
 
 	graphql "github.com/graph-gophers/graphql-go"
 	"github.com/graph-gophers/graphql-go/relay"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/rs/cors"
 )
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("Error loading .env file")
+	}
+
+	connString := fmt.Sprintf("host=localhost port=5432 user=%s dbname=%s password=%s sslmode=disable", os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PASS"))
+
+	DB, err := sql.Open("postgres", connString)
+	if err != nil {
+		log.Fatalln("Error: The data source arguments are not valid")
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		log.Fatalln("Error: Could not establish a connection with the database")
+	}
+}
+
 
 func main() {
 	opts := []graphql.SchemaOpt{graphql.UseFieldResolvers()}
